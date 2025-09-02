f56b42905899ce51a4781fcba7e0d648c2b4cfc5
Killua <79746314+Panossntjean@users.noreply.github.com>
2025-09-02 09:49:08 +0300

    Delete rogue directory

--- rogue/items/weapons/BladeOfChaos.java
+++
@@ -1,9 +0,0 @@
-package rogue.items.weapons;
-import rogue.items.Weapon;
-
-
-public class BladeOfChaos extends Weapon {
-    public BladeOfChaos() {
-        super("Blade Of Chaos", 'S', 35, 0, 25, 0); 
-    }
-}
